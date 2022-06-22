6decebefda799d5305753976a06322eb30d7214c
B0ney <40839054+B0ney@users.noreply.github.com>
2022-06-22 12:05:06 +0100

    work on sample decompression

--- src/it/compression.rs
+++ src/it/compression.rs
@@ -4,6 +4,9 @@
 /// https://github.com/schismtracker/schismtracker/blob/master/fmt/compression.c
 /// https://wiki.multimedia.cx/index.php/Impulse_Tracker#IT214_sample_compression
 /// 
+/// GOLD
+/// https://github.com/nicolasgramlich/AndEngineMODPlayerExtension/blob/master/jni/loaders/itsex.c
+/// 
 /// TODO:
 /// 
 /// Find an IT module that uses compressed samples
@@ -14,11 +17,193 @@
 /// 
 
 /*
+* A sample is compressed if bit 3 in flag is set.
+
+* Compressed sample is split into blocks, when uncompressed is 0x8000 bytes (0x4000 for 16 bit samples)
+* Decompressed block (or failed to) is delta decoded 
+
+* Each block starts with a 16-bit length field, followed by the bitstream (LE)
+
+*/
+
+/*
 Observation from C code
 
 
 
 */
 
-pub fn decompress_8bit(len: u32) {}
-pub fn decompress_16bit(len: u32) {}
+use crate::utils::Error;
+use byteorder::{ByteOrder, LE, BE};
+
+struct BitReader {
+    bitnum: u8,
+    bitlen: u32,
+    ival: u8, // internal value
+    ibuf_index: usize, // internal value pointer?
+}
+
+//  Easier to read
+impl BitReader { 
+    fn new() -> Self {
+        Self { 
+            bitnum: 0,
+            bitlen:0,
+            
+            ibuf_index: todo!(),
+            ival: todo!(),
+        }
+    }
+    fn read_bits(
+        &mut self,
+        width: u8,
+        // buf: &[u8],
+        // index: usize,
+    ) -> Result<u16, Error> {
+        let mut retval: u32 = 0;
+        // let mut offset: u32 = 0;
+        // let mut n = width;
+        
+        // // shadowing
+        // let mut index = index;
+        
+        // self.ival = buf[index];
+        // self.ibuf_index = index;
+
+        // while n != 0 {
+        //     let ival =  self.ival as u32;
+        //     let m = n; // set mask to copy of width
+
+        //     if self.bitlen == 0 {
+        //         return Err("ran out of buffer".into()); 
+        //     }
+
+        //     if m > self.bitnum {
+        //         m = self.bitnum
+        //     };
+
+        //     // Needs fixing
+        //     retval |= ival & ((1 << m) - 1) << offset;
+        //     self.ival >>= m;
+        //     n - m;
+        //     offset += m as u32;
+
+        //     // must check
+        //     self.bitnum -= m;
+
+        //     if self.bitnum == 0 {
+        //         self.bitlen -= 1;
+        //         self.ibuf_index += 1;
+        //         self.bitnum = 8;
+        //     };
+        // }
+            
+        Ok(retval as u16)
+    }   
+}
+
+
+
+/// Decompresses 8 bit sample from buffer.
+/// 
+/// At this stage, i'm not interested in optimisations
+/// 
+/// Think of this as a rustified version of itsex.c
+/// 
+/// The goal here is to achive simplicity.
+pub fn decompress_8bit(buf: &[u8], len: u32) -> Result<Vec<u8>, Error> {
+    let mut dest_buf: Vec<u8> = Vec::new(); // Buffer to write decompressed data
+
+    let mut len: u32 = len;     // Length of uncompressed sample. (copied for mutation)
+    let mut d1: i16 = 0;        // integrator buffer for IT2.14
+    let mut d2: i16 = 0;        // second integrator buffer for IT2.15
+
+    let mut blklen: u16;        // block length. Usually 0x8000 for 8-Bit samples
+    let mut blkpos: u16;        // block position
+
+    let mut width: u8;          // Bit width. Starts at 9 For 8-Bit samples.
+    let mut value: u16;         // Value read 
+
+    let mut bitread = BitReader::new();
+    
+    // Unpack data
+    while len != 0 {
+        // Read new block, reset variables
+        // {}
+        // Make sure block len won't exceed len.
+        blklen = if len < 0x8000 {len as u16} else {0x8000};
+        blkpos = 0;
+
+        width = 9;      //
+        
+        // Reset integrator buffers
+        d1 = 0; 
+        d2 = 0;
+
+        while blkpos < blklen {
+            value = bitread.read_bits(width)?;
+
+            if width < 7 { // Method 1, 1-6 bits
+                if value == (1 << (width - 1)) as u16
+                {
+                    value = bitread.read_bits(3)? + 1;
+
+                    let val = value as u8;
+                    width = if val < width { val } else { val + 1 };
+                    continue;
+                }
+            
+            } else if width < 9 { // Method 2, 7-8 bits
+                let border: u16 = (0xff >> (9 - width)) - 4;
+
+                if value > border
+                    && value <= (border + 8)
+                    {
+                        value -= border;
+
+                        let val = value as u8;
+                        width = if val < width { val } else { val + 1 };
+                        continue;
+                    }
+
+            } else if width == 9 {  // Method 3, 9 bits
+                if value & 0x100 == 1 // is the 9th bit set?
+                { 
+                    width = (value + 1) as u8;
+                    continue;
+                }
+                
+            } else {
+               return Err("Illegal width".into()); 
+            }
+
+            let mut v: i16;
+
+            // expand value to signed byte
+            if width < 8 {
+                let shift: u8 = 8 - width;
+                v = (value as i16) << shift;
+                v >>= shift
+            } else {
+                v = value as i16
+            }
+
+            // integrate
+            d1 += v;
+            d2 += d1;
+
+            // Push to buffer
+            {
+                let mut buf = vec![0u8 ;2];
+                LE::write_i16(&mut buf, d1); // todo
+                dest_buf.append(&mut buf);
+            }
+        
+            blkpos += 1;
+        }
+        len -= blklen as u32;
+    }
+    Ok(dest_buf)
+}
+// 
+// pub fn decompress_16bit(buf: &[u8], len: u32) -> Result<Vec<u8>, Error> {}
--- src/it/test.rs
+++ src/it/test.rs
@@ -1,4 +1,6 @@
-use std::fs::DirEntry;
+use std::{fs::DirEntry, vec};
+
+use crate::offset_u16;
 
 use super::it::*;
 
@@ -33,21 +35,249 @@ fn test_flag_set_2() {
 }
 #[test]
 fn test_dump() {
-    let a = ItFile::load("samples/beyond_-_darkcaribbean.it").unwrap();
+    let a = ItFile::load("samples/comp/before_the_explozion.it").unwrap();
     // let _ = a.export(format!("./test/test_dump_comp1.wav"), 6);
     for (i,f) in a.samples_meta.iter().enumerate() {
-        let _ = a.export(format!("./test/{i}.wav"), i);
+        // let _ = a.export(format!("./test/{i}.wav"), i);
 
         println!("{}",i);
-        println!("index: 0x{:04X}\nlength:{}\nrate:{}\n\n",
+        println!("index: 0x{:04X}\nlength:{}\nrate:{}\nbits smp: {}\ncompressed: {}\n\n",
             f.smp_ptr,
             f.smp_len,
-            
             f.smp_rate,
+            f.smp_bits,
+            f.smp_comp,
         );
 
     }
 }
+
+
+
+
+#[test]
+fn test_decomp() {
+    use byteorder::{ByteOrder, BE, LE};
+    let mut comp_data: Vec<u8> = std::fs::read("./test/raw/explosion_comp_smp_6").unwrap();
+    let mut len = comp_data.len(); //length of sample data
+    /*
+    length: 7411
+    bits smp 16
+    */
+    // let src_buff:
+    let smp_bits: u16 = 16;
+    /*
+    change to:
+    if 0x4000 < len {0x4000} else {len}
+    */
+    let block_len: u16 = 0x4000; // length of compressed data block in samples. 0x4000 because sample is 16 bit, otherwise it's 0x8000, 
+    let mut block_pos: u16 = 0;
+
+    let mut width: u8 = 17; // 17 because sample is 16 bit, otherwise start with 9
+
+    let mut value: u32; // value read from data to be processed
+
+    let mut len = comp_data.len(); //length of sample data
+    let mut dest_buf: Vec<u8> = vec![0; len];
+    let mut dest_pos: usize = 0;
+
+    let mut bitnum: u32 = 0;
+    let mut bitbuf: u32 = 0;
+    
+    println!("0x{:04X}", LE::read_u16(&comp_data[offset_u16!(0x0000)]));
+    // read block
+    /* 
+    block structure:
+    size: [u16]
+    
+    
+
+    */
+
+    // set value to ???
+
+    // reset integrator buffers
+    let mut d1 = 0;
+    let mut d2 = 0;
+    //  current position in source buffer
+    let mut srcbuf: usize = 0;// we'd set this to the sample pointer, but in this case set to 0
+    let mut filebuf: usize = 0;
+    
+
+    while (len != 0) {
+        // read new block, reset vars
+        d1 = 0;
+        d2 = 0;
+
+        if srcbuf + 2 > filebuf + len
+            || srcbuf + 2 /*+? */ > filebuf + len{
+
+            break;
+        };
+
+        // test decompress for 16 bit
+        while block_pos < block_len {
+            if width > 17 {
+                println!("Illegal bit width for 16 bit sample");
+                return;
+            } 
+
+            value = (it_readbits(
+                4,
+                &mut bitbuf,
+                &mut bitnum,
+                &comp_data,
+                srcbuf
+            ) + 1) as u32;
+
+            if width < 7 {
+                // Type A: Bit widths 1-6.
+                if value == 1 << (width - 1) as u32 {
+                    // read 4 bits for calculating new bit width
+                    value = (it_readbits(
+                        4,
+                        &mut bitbuf,
+                        &mut bitnum,
+                        &comp_data,
+                        srcbuf
+                    ) + 1);
+                    // expand
+
+                    width = if (value < width as u32) { value  as u8} else { (value + 1) as u8}; // check
+                    continue
+                } 
+
+            } else if width < 17 {
+                // Type B: Bit widths 7-16. (16 will be 8 if 8-bit )
+                let border: u32 = (0xffff >> (17 - width)) as u32 - 8;
+                
+                if value > border
+                    && value <= (border + 16) as u32
+                {
+                    value -= border as u32; // convert width to 1-8
+                    width = if (value < width as u32) { value as u8} else { (value + 1) as u8}; // check
+                    continue
+                }
+
+            } else {
+                // Type C:
+                // If top bit is set i.e 1, width = lower 8 bits + 1
+                // 0b00001_0000_0000_0000_0000
+                if value & 0x10000 ==1 {
+                    width = ((value+ 1) & 0x00ff) as u8;
+                    continue
+                }
+            }
+            // expand value to signed word:
+            let mut v: u16;
+            if width < 16 {
+                let shift = 16 - width;
+                v = (value as u16) << (shift as u16);
+                v >>= shift;
+            } else {
+                v = value as u16; // idk if this is good, we want to truncate 32 bit value to fit.
+            }
+            // "integrate upon the sample values"
+            d1 += v;
+            d2 += d1;
+
+
+            LE::write_u16(&mut dest_buf[offset_u16!(dest_pos)], d1);
+            // dest_buf.push(d1); // or d2?
+            dest_pos += 1; // only 1 channel
+            block_pos += 1;
+
+            // then store to buffer
+        }
+        len -= block_len as usize
+    }
+
+    println!("{}", dest_buf.len()); 
+    // instead of returning slice, why not return new vector containing decompressed sample?
+}
+
+// given width, and
+// fn it_readbits(
+//     width: u8,
+
+//     bit_buffer: &mut u32, 
+//     bit_num: &mut u32,
+
+//     buf: &[u8],
+// ) -> u32 {
+//     let mut value: u32 = 0;
+//     let i = width;
+
+//     for _ in i..0 {
+//         if (!bit_num) {
+//             // bit_buffer = ;
+//             bit_num = 8;
+//         } 
+//         value >>= 1;
+//         value |= bit_buffer << 31;
+//         bit_buffer >> 1;
+//         bit_num -= 1;
+//     }
+//     return value >> (32 - n); 
+
+// }
+
+
+/* 
+understaning code implementation
+
+in a nutshell:
+    * decompress data
+    * delta decode data
+
+delta decoding:
+A bit transition from 0->1 / 1->0 will be a 1
+no bit transition = 0
+
+1001_0110_0010_1001_0010 
+becomes:
+0101_1101_0011_1001_1011
+
+
+
+compressed block starts with 16 bit length field (2 bytes), followed by LE bitstream.
+
+BUF = buffer containing compressed sample
+
+Index = index for our buffer, this is equivalent to the "srcbuf" pointer in C
+"srcbuf" -> current position in source buffer.
+
+BUF[Index] = "*srcbuf" in C, provides u8.
+
+
+
+in loop:
+
+first 2 bytes are skipped, (Index += 2)
+
+block pos set to 0 (relative to 2nd skipped byte from above)
+
+
+
+
+
+
+dest_buf = Vec<u8> storing decompressed data
+
+dest_index = "destpos" in C
+
+dest_buf[dest_index] = "*destpos" in C
+
+"*destpos = it215 ? d2 : d1;" can be:
+if it215?
+    LE::write_u16(dest_buf[offset_u16!(index)], d2)
+else: 
+    LE::write_u16(dest_buf[offset_u16!(index)], d2)
+
+"destpos += channels;" -> dest_index += channels (mostly 1);
+
+
+*/
 
 // #[test]
 // fn find_compressed_sample() {
@@ -69,3 +299,70 @@ fn test_dump() {
 //         println!{"{:#?}", &cm};
 //     }
 // }
+
+#[test]
+fn read_bit_test() {
+    // 00000000000000000_010_1010_1100_1100
+    let buffer: Vec<u8> = vec![0b0010_1010, 0b1100_1100,0b0010_1010, 0b1100_1100];
+    let index: usize = 0;
+    let mut bitnum: u32 = 0;
+    let mut bitbuf: u32 = 0;
+    let bits_to_read: i8 = 15; 
+
+    let bits = it_readbits(
+        bits_to_read,
+        &mut bitbuf,
+        &mut bitnum,
+        &buffer,
+        index,
+    );
+
+
+    let bits_to_read: i8 = 16; 
+    let index: usize = 1;
+
+    let bits = it_readbits(
+        bits_to_read,
+        &mut bitbuf,
+        &mut bitnum,
+        &buffer,
+        index,
+    );
+
+
+    println!("{:032b}", bits);
+
+}
+// todo, make improved version as this is a rust mplementation of the c code
+// reads bits given buffer,
+// LE
+fn it_readbits(
+    n: i8,
+    bitbuf: &mut u32,
+    bitnum: &mut u32,
+    buf: &[u8],
+    index: usize,
+) -> u32 {
+    let mut index = index;
+    let mut value: u32 = 0;
+    let mut i: u32 = n as u32;
+    while i > 0 {
+        if (*bitnum) == 0 {
+            index += 1;
+            *bitbuf = buf[index] as u32;
+            *bitnum = 8;
+        }
+        value >>= 1;
+        value |= ( *bitbuf ) << 31;
+        (*bitbuf) >>= 1;
+        *bitnum -= 1;
+
+        i -= 1;
+    }
+    println!("current number:   {:032b}", bitnum);
+    println!("current bitbuffer: {:032b}", bitbuf);
+
+    return value >> (32 -n);
+
+}
+
