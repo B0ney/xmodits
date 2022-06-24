2d5476b97bc0fe3f7b6fdbb25a80381369ce40fb
B0ney <40839054+B0ney@users.noreply.github.com>
2022-06-24 16:26:09 +0100

    finished implementing mod sample dumping, now i need to clean up code

--- src/modtrk/modtrk.rs
+++ src/modtrk/modtrk.rs
@@ -1,23 +1,24 @@
 use std::path::Path;
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
+
 use byteorder::{BE,ByteOrder};
 use crate::{offset_chars, offset_u32, offset_u16};
 use crate::utils::{
     Error,
     wav,
-    array::load_to_array
+    array::load_to_array,signed::SignedByte
 };
 
 const MOD_SMP_START: usize = 0x0014;
 const MOD_SMP_LEN: usize = 0x1e;        // Sample data is 30 bytes in size
-// const MOD_SMP_PCM: uszie = ; // index where pcm data starts
-
+const PAT_META: usize = 0x3b8;
 pub struct MODFile {
     buf: Vec<u8>,
     title: [char; 20],
     smp_num: u8,
     smp_data: Vec<MODSample>,
-    // pat_num: u8,
+
 }
 
 impl MODFile {
@@ -37,16 +38,24 @@ impl MODFile {
 
         /// to find the number of patterns, 
         /// we select 128 bytes before offset 0x0438,
-        /// find the higest value, add 1.
+        /// find the highest value, add 1.
         /// 
         /// Use this (0x0438 + value * 1024) to skip pattern data
         /// which will lead us to the sample data
         /// 
         /// sample data is placed sequentially.
+        let smp_index: usize = {
+            0x0438 +
+            (*buf[offset_chars!(PAT_META, 128)]
+                .iter()
+                .max()
+                .unwrap() as usize + 1) * 1024 
+        };
+
 
         Ok(Self {
             title,
-            smp_data: build_samples(smp_num, &buf), 
+            smp_data: build_samples(smp_num, &buf, smp_index), 
             smp_num,
             // pat_num: &buf[0x03b6],
             buf
@@ -59,32 +68,42 @@ impl MODFile {
         let wav_header = wav::build_header(
             8363,
             8,
-            smp.smp_len,
+            smp.length as u32,
             false,
         );
 
         let mut file    = File::create(path)?;
         file.write_all(&wav_header)?;
+        let start = smp.index;
+        let end: usize = start + smp.length as usize;
+        println!("start: {}\nend: {}\n\n", start, end);
 
+        let pcm = (&self.buf[start..end]).to_signed();
+        file.write_all(&pcm)?;
 
         Ok(())
     }
 }
 
-fn build_samples(smp_num: u8, buf: &[u8]) -> Vec<MODSample> {
+fn build_samples(smp_num: u8, buf: &[u8], smp_start: usize) -> Vec<MODSample> {
     let mut smp_data: Vec<MODSample> = Vec::new();
     let smp_start_index: usize = MOD_SMP_START;
-    // let mut smp_pcm_stream_index = ;
+    let mut smp_pcm_stream_index = smp_start;
+    // generate sample index here
 
     for i in 0..smp_num as usize {
         let index = smp_start_index + (i * MOD_SMP_LEN); 
+        let len = BE::read_u16(&buf[offset_u16!(0x0016 + index)]) * 2; // Double to get size in bytes
         let mut name: [char; 22] = [' '; 22];
 
         load_to_array(&mut name, &buf[offset_chars!(index, 22)]);
         smp_data.push(MODSample {
+            index: smp_pcm_stream_index,
             name,
-            length: BE::read_u16(&buf[offset_u16!(0x0016 + index)]) * 2, // Double to get size in bytes
-        })
+            length: len, 
+        });
+
+        smp_pcm_stream_index += len as usize;
     }
     
     smp_data
@@ -93,15 +112,18 @@ fn build_samples(smp_num: u8, buf: &[u8]) -> Vec<MODSample> {
 pub struct MODSample {
     name: [char; 22],
     length: u16,    // multiply by 2 to get length in bytes
-    // start_index: usize
+    index: usize
 }
 
 #[test]
 fn test1() {
-    let protk = MODFile::load("samples/mod/omnibus.mod").unwrap();
-    println!("no. allocated samples: {}\n\n", protk.smp_num);
+    let protk = MODFile::load("samples/mod/wicked_time.mod").unwrap();
+    println!("no. allocated samples: {}\n\n", protk.buf.len());
 
-    for i in protk.smp_data {
-        println!("{}", i.length);
+    for (index, i) in protk.smp_data.iter().enumerate().filter(|(_,e)| e.length != 0) {
+        if let Err(e) = protk.export(format!("test/mod/{}.wav", index), index) {
+            println!("{:?}", e);
+        }
+        // println!("index: {} size: {}", i.index, i.length);
     }
 }
