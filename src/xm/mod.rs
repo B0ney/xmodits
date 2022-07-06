4388fdc068e60e37c5cfbafcf82bdfc20f105591
B0ney <40839054+B0ney@users.noreply.github.com>
2022-07-06 12:10:34 +0100

    xm milestone: obtained enough infomation to dump samples

--- src/xm/mod.rs
+++ src/xm/mod.rs
@@ -6,7 +6,7 @@ use crate::utils::prelude::*;
 const XM_HEADER_ID: &str    = "Extended Module: ";
 const XM_MAGIC_NUM: u8      = 0x1a;
 const XM_MIN_VER: u16       = 0x0104;
-const XM_SMP_BITS: u8       = 0b0000_1000;  // 1 = 16 bit samples
+const XM_SMP_BITS: u8       = 0b0001_0000;  // 1 = 16 bit samples
 
 #[derive(Debug)]
 pub struct XMSample {
@@ -15,6 +15,7 @@ pub struct XMSample {
     smp_flags: u8,      
     smp_bits: u8,       // bits per sample
     smp_ptr: usize,
+    smp_rate: i32,
 }
 
 pub struct XMFile {
@@ -54,7 +55,6 @@ impl TrackerDumper for XMFile {
         // Pattern headers do not have a fixed size so we need to calculate
         // their total size and add that to 0x0150
         let ins_header_offset: usize = skip_pat_header(&buf, patnum as usize);
-
 
         // with the offset given by ins_header_offset,
         // we can obtain infomation about each instrument
@@ -106,7 +106,6 @@ fn skip_pat_header(buf: &[u8], patnum: usize) -> usize {
         assert_eq!(pat_header_len, 9, "header len is not 9?");
         offset += (pat_header_len + pat_data_size) as usize; 
     }
-
     offset as usize
 }
 
@@ -131,72 +130,53 @@ fn build_samples(
             offset += header_size as usize;
             continue;
         };
-
         // Obtain additional infomation from 
         // instrument header
         smp_header_size = LE::read_u32(&buf[dword!(0x001d + offset)]); // should be 40?
         
         offset += header_size as usize; // skip additional header to sample headers ()
 
-        // (length, flag, name)
-        let mut smp_info: Vec<(u32, u8, String)> = Vec::new();
+        // (length, flag, name, finetune, relative note number)
+        let mut smp_info: Vec<(u32, u8, String, i8, i8)> = Vec::new();
 
         // Sample header follows after additional header
+        // When this loop completes, the offset will land at sample data
         for _ in 0..ins_smp_num {
             smp_info.push((
                 LE::read_u32(&buf[dword!(0x0000 + offset)]),
                 buf[0x000e + offset],
-                string_from_chars(&buf[chars!(0x0012 + offset, 22)])
+                string_from_chars(&buf[chars!(0x0012 + offset, 22)]),
+                buf[0x000d+ offset] as i8,
+                buf[0x0010+ offset] as i8,
             ));
+            
             offset += smp_header_size as usize
         }
-        // When the loop completes, the offset will land at sample data
-        // 
+        
+        for (
+            smp_len,
+            smp_flags,
+            smp_name,
+            finetune,
+            notenum,
+        ) in smp_info {
+            let period: f32     = 7680.0 - ((48.0 + notenum as f32) * 64.0) - (finetune as f32 / 2.0);
+            let smp_rate: i32   = (8363.0 * 2.0_f32.powf((4608.0 - period) / 768.0)) as i32;
 
-        for (smp_len, smp_flags, smp_name) in smp_info {
             samples.push(XMSample{
                 smp_bits: (((smp_flags & XM_SMP_BITS) >> 4) + 1) * 8,
                 smp_len: smp_len as usize,
                 smp_name,
                 smp_flags,
                 smp_ptr: offset,
+                smp_rate
             });
+
             offset += smp_len as usize;
         }
-    } 
+    }
 
     Ok(samples)
-    
-        
-        
-        
-        
-
-    // println!("header size: {}\ninstrument name: {}\nsample number: {}\n",
-    //     header_size,
-    //     ins_name,
-    //     ins_smp_num,
-    // );
-
-
-    /*
-        repeat insnum
-        
-        If number of samples is zero, skip HEADER_SIZE bytes (skip to next xm ins)
-        
-        IF smp num is > 0, obtain additional infomation from xm header
-        Obtain infomation from sample headers,
-        
-        calculate parapointers to sample data
-        this can be calculated given current offset + HEADER size 
-        as it includes total size of sample headers.
-        
-        build xm sample structs
-    */
-    
-
-    // let mut header
-    // todo!()
 }
 
 #[test]
