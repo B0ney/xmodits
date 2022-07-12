f433e8ee473fb608b5a75113e9d188d8d82ee51d
B0ney <40839054+B0ney@users.noreply.github.com>
2022-07-12 11:08:47 +0100

    test build script

--- app_cli/build.rs
+++ app_cli/build.rs
@@ -1,12 +1,50 @@
-use std::io;
-#[cfg(windows)] use winres::WindowsResource;
+use std::env;
+use std::path::Path;
+use std::path::PathBuf;
+use version_check as rustc;
 
-fn main() -> io::Result<()> {
-    #[cfg(windows)] {
-        WindowsResource::new()
-            // This path can be absolute, or relative to your crate root.
-            .set_icon("../../extras/logo/icon.ico")
-            .compile()?;
+fn main() {
+    // if !rustc::is_max_version("1.60").unwrap() {
+    //     panic!("This app requires Rust 1.60 or older.");
+    // }
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        use winres::WindowsResource;
+        let mut ws = WindowsResource::new();
+        ws.set_icon_with_id("./src/icon.ico", "icon");
+        ws.set_toolkit_path("/usr/bin")
+            .set_windres_path("x86_64-w64-mingw32-windres")
+            .set_ar_path("x86_64-w64-mingw32-ar");
+        ws.compile().unwrap();
     }
-    Ok(())
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    // let assets = ["icon.png", "icon.ico"];
+    // for asset in assets.iter() {
+    //     println!("cargo:rerun-if-changed=./src/{}", asset);
+    // }
+    // println!("cargo:rerun-if-changed=./src/icon.png");
+    // println!("cargo:rerun-if-changed=./src/icon.ico");
+
+    // let output_path = get_output_path();
+    // let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // let input_path = Path::new(&manifest_dir);
+
+    // for asset in assets.iter() {
+    //     std::fs::copy(
+    //         input_path
+    //             .join("./src/".to_string() + asset)
+    //             .to_str()
+    //             .unwrap(),
+    //         output_path.join(asset),
+    //     )
+    //     .unwrap();
+    // }
 }
+// fn get_output_path() -> PathBuf {
+//     //<root or manifest path>/target/<profile>/
+//     let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
+//     let build_type = env::var("PROFILE").unwrap();
+//     let path = Path::new(&manifest_dir_string)
+//         .join("target")
+//         .join(build_type);
+//     return PathBuf::from(path);
+// }
--- build-multi.sh
+++ build-multi.sh
@@ -1,1 +1,2 @@
+rustup override set 1.60
 cargo +nightly build --features="ascii_art" -p xmodits -Zmultitarget --target=x86_64-unknown-linux-gnu --target=x86_64-pc-windows-gnu --release
