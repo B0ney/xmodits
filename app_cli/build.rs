143d389e79b8e7f6cded9a04f7caa6d74dd39b27
B0ney <40839054+B0ney@users.noreply.github.com>
2022-07-13 13:39:54 +0100

    Dev (#5)
    * update readme
    
    * remove clap, binary size reduced to 50% of previous size

--- app_cli/build.rs
+++ app_cli/build.rs
@@ -6,7 +6,7 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         use winres::WindowsResource;
         let mut ws = WindowsResource::new();
-        ws.set_icon_with_id("./src/icon.ico", "icon");
+        ws.set_icon_with_id("../extras/logos/icon.ico", "icon");
         ws.set_toolkit_path("/usr/bin")
             .set_windres_path("x86_64-w64-mingw32-windres")
             .set_ar_path("x86_64-w64-mingw32-ar");
--- app_cli/src/main.rs
+++ app_cli/src/main.rs
@@ -1,49 +1,70 @@
 // #![windows_subsystem = "windows"]
 mod app;
-use std::path::{Path, PathBuf};
+use std::env;
+use std::path::PathBuf;
 use xmodits_lib::Error;
-use clap::{Command, arg, crate_version, crate_authors};
 
+#[cfg(feature="ascii_art")] const LOGO: &str = include_str!("../../extras/ascii_art.txt");
+#[cfg(not(feature="ascii_art"))] const LOGO: &str = "xmodits";
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+const HELP: &str = "
+USAGE:
+  xmodits <module(s)> [destination folder]
+
+FLAGS:
+  -h, --help            Prints help information
+  -v, --version         Prints version
+
+EXAMPLES:
+    xmodits song1.s3m
+
+    xmodits song1.s3m ~/Downloads/
+
+    xmodits song1.s3m song2.it 
+
+    xmodits song1.s3m song2.it ~/Downloads/
+";
+
+fn help() {
+    println!("{LOGO}-{VERSION}");
+    println!("By {AUTHOR}");
+    println!("{HELP}");
+}
+
+fn version() {
+    println!("{VERSION}");
+}
+fn toal_size(paths: Vec<PathBuf>) -> u64 {
+    paths
+        .iter()
+        .map(|e| if let Ok(m) = e.metadata() {m.len()} else {0}).sum()
+
+}
 fn main() -> Result<(), Error> {
-    let matches = Command::new(
-            if cfg!(feature="ascii_art") {
-                include_str!("../../extras/ascii_art.txt")
-            } else {
-                "xmodits"
-            }            
-        )
-        .about("Sample dumping tool for tracker modules.")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .arg(
-            arg!(<module> "Path to tracker module, the last argument is the destination folder.")
-                .required(true)
-                .multiple_values(true)
-        )
-        .get_matches();
-        
-    let mod_path = match matches.get_one::<String>("module"){
-        Some(path) => { PathBuf::new().join(path) },
-        None => unimplemented!(),
-    };
+    let args: Vec<std::ffi::OsString> = env::args_os().skip(1).collect();
 
-    let mut paths: Vec<PathBuf> = matches.get_many::<String>("module")
-        .unwrap()
-        .cloned()
-        .map(|f| PathBuf::from(f))
-        .collect();
+    if args.len() == 0 { help(); return Ok(()); }
+
+    match args[0].to_str() {
+        Some("-v") | Some("--version")  => { version(); return Ok(()); },
+        Some("-h") | Some("--help")     => { help(); return Ok(()); },
+        _ => {},
+    }
+
+    let mut paths: Vec<PathBuf> = args.iter().map(|f| PathBuf::from(f)).collect();
 
     let dest_dir: PathBuf = match paths.last().unwrap() {
         p if p.is_dir() && paths.len() > 1 => paths.pop().unwrap(),
-        _ => std::env::current_dir()?,
+        _ => env::current_dir()?,
     };
-
+        
     paths
         .iter()
         .filter(|f| f.is_file())
         .for_each(|mod_path| {
             if let Err(e) = app::dump_samples(mod_path, &dest_dir) {
-                println!("{}", e);
+                eprintln!("Error: {}", e);
             }
         }
     );
