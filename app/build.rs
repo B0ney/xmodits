1d6d8260b5d9dfcfe919e3594ebe4bc36e1d9856
B0ney <40839054+B0ney@users.noreply.github.com>
2022-07-16 17:01:21 +0100

    cleanup icons

--- app/build.rs
+++ app/build.rs
@@ -6,9 +6,7 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         use winres::WindowsResource;
         let mut ws = WindowsResource::new();
-        ws.set_icon_with_id("../extras/logos/icon.ico", "icon")
-            .set_icon_with_id("../extras/logos/icon2.ico", "2")
-            .set_icon_with_id("../extras/logos/icon3.ico", "3");
+        ws.set_icon_with_id("../extras/logos/ico/icon.ico", "icon");
         #[cfg(unix)]
         ws.set_toolkit_path("/usr/bin")
             .set_windres_path("x86_64-w64-mingw32-windres")
