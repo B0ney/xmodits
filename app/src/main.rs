cd040d0e3c15e493f956506bd64e82f9600684aa
B0ney <40839054+B0ney@users.noreply.github.com>
2022-10-06 16:12:09 +0100

    re-add clap, work on allowing the user to set custom parameters for ripping

--- app/src/main.rs
+++ app/src/main.rs
@@ -14,52 +14,101 @@ mod app_win;
 #[cfg(target_os = "windows")]
 mod dialoge;
 
-fn main() -> Result<(), Error> {
-    // Collect arguments
-    let args: Vec<std::ffi::OsString> = env::args_os().skip(1).collect();
+// fn main() -> Result<(), Error> {
+//     // Collect arguments
+//     let args: Vec<std::ffi::OsString> = env::args_os().skip(1).collect();
 
-    // Show help to user if they launch the app with no arguments
-    // On Windows, this is a dialogue box
-    if args.len() == 0 { 
-        #[cfg(windows)]{ return Ok(dialoge::show_help_box()); }
+//     // Show help to user if they launch the app with no arguments
+//     // On Windows, this is a dialogue box
+//     if args.len() == 0 { 
+//         #[cfg(windows)]{ return Ok(dialoge::show_help_box()); }
 
-        #[cfg(unix)]{ return Ok(cli::help()); }
-    }
+//         #[cfg(unix)]{ return Ok(cli::help()); }
+//     }
 
-    // On *nix systems we quit the application if given:
-    // -v, --version, -h, --help
-    #[cfg(unix)]{ app_unix::check_args(&args); }
+//     // On *nix systems we quit the application if given:
+//     // -v, --version, -h, --help
+//     #[cfg(unix)]{ app_unix::check_args(&args); }
 
-    // Convert argument into a Vector of paths
-    let mut paths: Vec<PathBuf> = args
-        .iter()
-        .map(|f| PathBuf::from(f))
-        .collect();
+//     // Convert argument into a Vector of paths
+//     let mut paths: Vec<PathBuf> = args
+//         .iter()
+//         .map(|f| PathBuf::from(f))
+//         .collect();
     
-    // We treat the last argumet as the destination folder
-    // If the last argument is not a valid folder, make the destination
-    // folder the current executable directory.
-    let dest_dir: PathBuf = match paths.last().unwrap() {
-        p if p.is_dir() && paths.len() > 1 => paths.pop().unwrap(),
-        _ => env::current_dir()?,
-    };
+//     // We treat the last argumet as the destination folder
+//     // If the last argument is not a valid folder, make the destination
+//     // folder the current executable directory.
+//     let dest_dir: PathBuf = match paths.last().unwrap() {
+//         p if p.is_dir() && paths.len() > 1 => paths.pop().unwrap(),
+//         _ => env::current_dir()?,
+//     };
     
-    // Filter paths to just contain files.
-    let modules: Vec<PathBuf> = paths
-        .iter()
-        .filter(|f| f.is_file())
-        .map(|f| f.clone())
-        .collect();   
+//     // Filter paths to just contain files.
+//     let modules: Vec<PathBuf> = paths
+//         .iter()
+//         .filter(|f| f.is_file())
+//         .map(|f| f.clone())
+//         .collect();   
 
-    if modules.len() == 0 { 
-        #[cfg(windows)]{ return Ok(dialoge::no_valid_modules()); }
+//     if modules.len() == 0 { 
+//         #[cfg(windows)]{ return Ok(dialoge::no_valid_modules()); }
 
-        #[cfg(unix)]{ return Ok(cli::help()); }
-    }
+//         #[cfg(unix)]{ return Ok(cli::help()); }
+//     }
 
-    #[cfg(unix)]
-    return app_unix::run(&modules, &dest_dir); 
+//     #[cfg(unix)]
+//     return app_unix::run(&modules, &dest_dir); 
 
-    #[cfg(windows)]
-    return app_win::run(modules, dest_dir);
+//     #[cfg(windows)]
+//     return app_win::run(modules, dest_dir);
+// }
+
+
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+	paths: Vec<String>,
+
+    #[arg(help="Only name samples with an index. E.g. 01.wav")]
+    #[arg(short='i', long)]
+	index_only: bool,
+
+    #[arg(help="Preserve sample indexing")]
+    #[arg(short='r', long)]
+	index_raw: bool,
+
+    #[arg(help="Pad index with preceding 0s. e.g. 001, or 0001")]
+    #[arg(default_value_t = 2, short='p', long="index-padding", value_parser=0..=5)]
+	index_padding: i64,
+
+    #[arg(help="Include embedded text from tracker (if it exists)")]
+    #[arg(short='c', long)]
+    with_comment: bool,
+
+    #[arg(short, long)]
+	with_folder: bool,
+
+    #[arg(help="Name samples in UPPER CASE")]
+    #[arg(short, long="upper", conflicts_with="lower_case")]
+	upper_case: bool,
+
+    #[arg(help="Name samples in lower case")]
+    #[arg(short, long="lower", conflicts_with="upper_case")]
+	lower_case: bool,
+
+    #[cfg(feature="advanced")]
+    #[arg(help="Rip samples in parallel")]
+    #[arg(short='k', long)]
+	parallel: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    dbg!(cli.with_comment);
+    dbg!(cli.paths);
 }
