a8c8b6fe258cfe052c71e3e69922887824ddd563
B0ney <40839054+B0ney@users.noreply.github.com>
2022-10-11 12:37:24 +0100

    code cleanup, make modloader robust

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
 mod fmt;
-use std::{path::{Path, PathBuf}, collections::HashMap, hash::Hash};
-
-use fmt::*;
 #[allow(unused, dead_code)]
 mod utils;
 mod interface;
 mod formatter;
 mod error;
 
-pub use interface::{TrackerDumper, TrackerModule, TrackerSample, SampleNamerFunc};
+pub use interface::SampleNamerFunc;
+pub use interface::TrackerSample;
+pub use interface::TrackerModule;
+pub use interface::TrackerDumper;
+
+use std::collections::HashMap;
+use std::path::Path;
 pub use error::XmoditsError;
-use once_cell::sync::Lazy;
+
 pub use utils::Error;
 pub use formatter::SampleNamer;
+
+use fmt::*;
+use once_cell::sync::Lazy;
 
 pub mod tracker_formats {
     pub use crate::it::ITFile;
@@ -21,24 +27,6 @@ pub mod tracker_formats {
     pub use crate::s3m::S3MFile;
     pub use crate::umx::UMXFile;
 }
-
-/// Load a tracker module based on file extension
-pub fn load_module<P: AsRef<std::path::Path>>(path: P) -> Result<TrackerModule, XmoditsError> {
-    use tracker_formats::*;
-    match file_extension(&path).to_lowercase().as_str() {
-        "it"    => ITFile::load_module(path),
-        "xm"    => XMFile::load_module(path),
-        "s3m"   => S3MFile::load_module(path),
-        "mod"   => MODFile::load_module(path),
-        f   => Err(
-            XmoditsError::UnsupportedFormat(
-                format!("'{}' is not a supported format.", f)
-            )
-        ),
-    }
-}
-
-// https://stackoverflow.com/questions/65157092/how-to-construct-a-hashmap-with-boxed-fn-values
 
 type ModLoaderFunc = fn(&Path) -> Result<TrackerModule, XmoditsError>;
 
@@ -49,35 +37,37 @@ static LOADER: Lazy<HashMap<&str, ModLoaderFunc>> = Lazy::new(|| {
         ("xm", |p| XMFile::load_module(&p)),
         ("s3m", |p| S3MFile::load_module(&p)),
         ("mod", |p| MODFile::load_module(&p)),
+        // ("umx", |p| UMXFile::load_module(&p)),
     ];
     HashMap::from(b)
 });
 
-pub fn load_module_robust<P>(path: P) -> Result<TrackerModule, XmoditsError> 
+/// A more robust method to load a module gven a path.
+/// 
+/// Load a module given a file extension.
+/// 
+/// If it fails, loop through other module loaders, return if one succeeds.
+pub fn load_module<P>(path: P) -> Result<TrackerModule, XmoditsError> 
 where P: AsRef<std::path::Path>
 {
     let ext = file_extension(&path).to_lowercase();
     let path = path.as_ref();
 
     match LOADER.get(ext.as_str()) {
-        Some(mod_loader) => {
-            if let Ok(tracker) = mod_loader(&path) {
-                Ok(tracker)
-            } else {
-                for (extension, backup_loader) in LOADER.iter().filter(|k| k.0 != &ext.as_str()) {
-                    if let Ok(tracker) = backup_loader(&path) {
-                        println!("Detected: {}", extension);
+        Some(mod_loader) => match mod_loader(path) {
+            Ok(tracker) => Ok(tracker),
+            Err(original_err) => {
+                for (_, backup_loader) in LOADER.iter().filter(|k| k.0 != &ext.as_str() && k.0 != &"mod") {
+                    if let Ok(tracker) = backup_loader(path) {
                         return Ok(tracker);
                     } else {
                         continue
                     }
                 }
-                return Err(XmoditsError::UnsupportedFormat(
-                    format!("Could not determine a valid format from {}", path.display() )
-                ));
+                Err(original_err)
             }
         },
-        None => return Err(XmoditsError::UnsupportedFormat(format!("'{}' is not a supported format.", ext)))
+        None => Err(XmoditsError::UnsupportedFormat(format!("'{}' is not a supported format.", ext)))
     }
 }
 
@@ -92,7 +82,9 @@ fn file_extension<P: AsRef<std::path::Path>>(p: P) -> String {
 // Hashmaps are not sorted
 #[test]
 fn robust() {
-    let a= load_module_robust("./tests/mods/xm/DEADLOCK.s3m").unwrap();
-
+    let a= load_module("./tests/mods/xm/DEADLOCK.s3m");
+    if let Err(a) = a {
+        dbg!(a);
+    }
     dbg!();
 }
