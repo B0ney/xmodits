9043368c091cde9a36c6177da21a978ed16db8e0
B0ney <40839054+B0ney@users.noreply.github.com>
2023-04-05 10:47:38 +0100

    gui: write errors in subscription

--- xmodits-gui/src/core/xmodits.rs
+++ xmodits-gui/src/core/xmodits.rs
@@ -2,7 +2,7 @@ use iced::{subscription, Subscription};
 use std::path::PathBuf;
 use tokio::sync::mpsc::{self, Receiver, Sender, UnboundedReceiver, UnboundedSender};
 
-use super::cfg::SampleRippingConfig;
+use super::cfg::{self, Config, SampleRippingConfig};
 pub type StartSignal = (Vec<PathBuf>, SampleRippingConfig);
 const ID: &str = "XMODITS_RIPPING";
 
@@ -16,6 +16,8 @@ enum State {
         ripping_msg: UnboundedReceiver<ThreadMsg>,
         total: usize,
         progress: usize,
+        error_handler: Error,
+        errors: usize,
     },
 }
 
@@ -26,10 +28,12 @@ pub enum DownloadMessage {
     Done,
     Progress {
         progress: f32,
-        error: Option<Failed>,
+        errors: usize,
+        // error: Option<Failed>,
     },
     Info(Option<String>),
 }
+
 use super::extraction::{Failed, ThreadMsg};
 
 /// The subscription will emit messages when:
@@ -48,13 +52,19 @@ pub fn xmodits_subscription() -> Subscription<DownloadMessage> {
                     let total = config.0.len();
                     let (tx, rx) = mpsc::unbounded_channel();
 
-                    spawn_worker_thread(tx, config);
+                    std::thread::spawn(move || {
+                        let (paths, config) = config;
+                        super::extraction::rip(tx, paths, config);
+                    });
+
                     (
                         None,
                         State::Ripping {
                             ripping_msg: rx,
                             total,
                             progress: 0,
+                            error_handler: Error::default(),
+                            errors: 0,
                         },
                     )
                 }
@@ -64,19 +74,29 @@ pub fn xmodits_subscription() -> Subscription<DownloadMessage> {
                 mut ripping_msg,
                 total,
                 mut progress,
+                mut error_handler,
+                mut errors,
             } => match ripping_msg.recv().await {
                 Some(ThreadMsg::Progress(error)) => {
                     progress += 1;
                     let percentage: f32 = (progress as f32 / total as f32) * 100.0;
+
+                    if let Some(failed) = error {
+                        errors += 1;
+                        error_handler.push(failed).await;
+                    }
+
                     (
                         Some(DownloadMessage::Progress {
                             progress: percentage,
-                            error,
+                            errors,
                         }),
                         State::Ripping {
                             ripping_msg,
                             total,
                             progress,
+                            error_handler,
+                            errors,
                         },
                     )
                 }
@@ -86,6 +106,8 @@ pub fn xmodits_subscription() -> Subscription<DownloadMessage> {
                         ripping_msg,
                         total,
                         progress: 0,
+                        error_handler,
+                        errors,
                     },
                 ),
                 Some(ThreadMsg::Info(info)) => (
@@ -94,21 +116,117 @@ pub fn xmodits_subscription() -> Subscription<DownloadMessage> {
                         ripping_msg,
                         total,
                         progress,
+                        error_handler,
+                        errors,
                     },
                 ),
+                Some(ThreadMsg::Done) => {
+                    (Some(DownloadMessage::Done), State::Init)
+                }
+                // None => todo!(),
                 _ => (Some(DownloadMessage::Done), State::Init),
             },
         }
     })
 }
 
-// async fn rip(state: State) -> (Option<DownloadMessage>, State) {
+const MAX: usize = 150;
+const ABS_LIMIT: usize = MAX * 10;
 
-// }
+#[derive(Debug)]
+pub enum Error {
+    Mem {
+        errors: Vec<Failed>,
+        delegate: PathBuf,
+    },
+    File {
+        lines: usize,
+        path: PathBuf,
+        file: Box<tokio::fs::File>,
+    },
+    FailedFile {
+        reason: String,
+        errors: Vec<Failed>,
+        discarded: usize,
+    },
+}
 
-fn spawn_worker_thread(tx: UnboundedSender<ThreadMsg>, config: StartSignal) {
-    std::thread::spawn(move || {
-        let (paths, config) = config;
-        super::extraction::rip(tx, paths, config);
-    });
+impl Default for Error {
+    fn default() -> Self {
+        Self::Mem {
+            errors: Vec::new(),
+            delegate: dirs::download_dir().expect("downloads folder"),
+        }
+    }
+}
+use tokio::io::AsyncWriteExt;
+
+impl Error {
+    async fn push(&mut self, error: Failed) {
+        match self {
+            Error::Mem { errors, delegate } => {
+                if errors.len() < MAX {
+                    errors.push(error);
+                    return;
+                }
+
+                let mut errors = std::mem::take(errors);
+                let mut folder = std::mem::take(delegate);
+
+                errors.push(error);
+                folder.push("xmodits-error-log.txt");
+
+                *self = match tokio::fs::OpenOptions::new()
+                    .write(true)
+                    .create_new(true)
+                    .open(&folder)
+                    .await
+                    .map(|f| Box::new(f))
+                {
+                    Ok(mut file) => {
+                        let lines = errors.len();
+
+                        for error in errors {
+                            Self::write_error(&mut file, error).await;
+                        }
+
+                        Self::File {
+                            lines,
+                            path: folder,
+                            file,
+                        }
+                    }
+
+                    Err(error) => Self::FailedFile {
+                        reason: error.to_string(),
+                        errors,
+                        discarded: 0,
+                    },
+                };
+            }
+
+            Error::File { lines, file, .. } => {
+                Self::write_error(file, error).await;
+                *lines += 1;
+            }
+
+            Error::FailedFile {
+                errors, discarded, ..
+            } => {
+                if errors.len() < ABS_LIMIT {
+                    errors.push(error);
+                    return;
+                }
+                *discarded += 1;
+            }
+        }
+    }
+
+    async fn write_error(file: &mut tokio::fs::File, error: Failed) {
+        let failed_file = error.path.display().to_string();
+        let _ = file.write_all(failed_file.as_bytes()).await;
+        let _ = file.write_all(b"\n     ").await;
+        let _ = file.write_all(error.reason.as_bytes()).await;
+        let _ = file.write_all(b"\n").await;
+    }
 }
