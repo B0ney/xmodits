ee2847850a0b899ad1fa56a92fc261b9a50a5f42
B0ney <40839054+B0ney@users.noreply.github.com>
2023-06-09 23:36:38 +0100

    loop sample during playback

--- xmodits-gui/src/core/audio/sample.rs
+++ xmodits-gui/src/core/audio/sample.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::sync::{Arc, Weak};
 use xmodits_lib::dsp::sample::{FramesIter, SampleFrame};
 use xmodits_lib::dsp::{resampler::resample, RawSample, SampleBuffer};
+use xmodits_lib::interface::sample::{self, LoopType};
 
 pub struct SampleInfo {
     name: String,
@@ -18,9 +19,10 @@ pub struct AudioDevice {
 
 #[derive(Clone)]
 pub struct TrackerSample {
-    buffer: Arc<SampleBuffer>,
+    pub buffer: Arc<SampleBuffer>,
     frame: usize,
     reversed: bool,
+    looping: bool,
 }
 
 impl TrackerSample {
@@ -29,26 +31,67 @@ impl TrackerSample {
             buffer: sample.into(),
             frame: 0,
             reversed: false,
+            looping: false,
+        }
+    }
+
+    pub fn end_frame(&self) -> usize {
+        match self.buffer.loop_data.is_disabled() {
+            true => self.buffer.duration() - 1,
+            false => self.buffer.end(),
+        }
+    }
+
+    pub fn start_frame(&self) -> usize {
+        match self.buffer.loop_data.is_disabled() {
+            true => 0,
+            false => self.buffer.start(),
+        }
+    }
+
+    pub fn hit_barrier(&self) -> bool {
+        if self.frame >= self.buffer.duration() || self.frame >= self.end_frame() {
+            return true;
+        }
+
+        match self.looping {
+            false => self.frame == 0 && self.reversed,
+            true => self.frame <= self.start_frame(),
         }
     }
 
     pub fn reverse(&mut self) {
         self.reversed = !self.reversed;
 
-        if self.frame == 0 || self.frame >= self.buffer.duration() - 1 {
-            self.frame = match self.reversed {
-                true => self.buffer.duration() - 1,
-                false => 0,
-            }
-        };
+        self.frame = match self.buffer.loop_type() {
+            LoopType::Forward => self.start_frame(),
+            LoopType::Backward | LoopType::PingPong => match self.reversed {
+                true => self.end_frame() - 1,
+                false => self.start_frame(),
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn rate(&self) -> usize {
+        self.buffer.rate as usize
+    }
+
+    pub fn rate_original(&self) -> usize {
+        self.buffer.rate_original() as usize
     }
 }
 
 impl PlayHandle for TrackerSample {
     fn next(&mut self) -> Option<[f32; 2]> {
-        if self.frame >= self.buffer.duration() || self.frame == 0 && self.reversed {
-            // self.reverse();
-            return None;
+        if self.hit_barrier() {
+            match self.buffer.loop_data.is_disabled() {
+                true => return None,
+                false => {
+                    self.looping = true;
+                    self.reverse();
+                }
+            }
         }
 
         let result = self.buffer.frame(self.frame).map(|f| f.get_stereo_frame());
@@ -62,7 +105,8 @@ impl PlayHandle for TrackerSample {
     }
 
     fn reset(&mut self) {
-        self.frame = 0
+        self.frame = 0;
+        self.looping = false;
     }
 
     fn jump(&mut self, tick: usize) {
