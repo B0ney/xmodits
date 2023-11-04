a1f2b8f74b534b967ddf15dab958bbab0b381de9
B0ney <40839054+B0ney@users.noreply.github.com>
2023-11-04 18:39:51 +0000

    test

--- dist/build-macos.sh
+++ dist/build-macos.sh
@@ -63,4 +63,5 @@ ls $ARCHIVE_DIR
 ARCHIVE_NAME="xmodits-gui-v$VERSION-$PLATFORM-universal.zip"
 ARCHIVE_PATH="$ARTIFACT_DIR/$ARCHIVE_NAME"
 
-zip -r $ARCHIVE_PATH $ARCHIVE_DIR
+# zip -r $ARCHIVE_PATH $ARCHIVE_DIR
+ditto -c -k --sequesterRsrc --keepParent $ARCHIVE_DIR $ARCHIVE_PATH
