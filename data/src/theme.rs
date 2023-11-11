ba6e15230a8abc288433b9927f10a236193c6abb
B0ney <40839054+B0ney@users.noreply.github.com>
2023-11-11 12:29:23 +0000

    rewrite palette code and refactor theming

--- data/src/theme.rs
+++ data/src/theme.rs
@@ -1,7 +1,7 @@
 pub mod themes;
 
 use iced_core::Color;
-// use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 pub use themes::Themes;
 
 pub struct Theme {
@@ -11,37 +11,111 @@ pub struct Theme {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Palette {
-    pub base: BaseColors,
-    pub normal: NormalColors,
-    pub bright: BrightColors,
+    pub background: Color,
+    pub middleground: Color,
+    pub foreground: Color,
+    pub border: Color,
+    pub text: Color,
+    pub accent: Color,
+    pub error: Color,
+    pub warning: Color,
+    pub success: Color,
 }
 
 impl Default for Palette {
     fn default() -> Self {
-        Themes::Catppuccin.palette()
+        themes::Themes::Catppuccin.palette()
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct BaseColors {
-    pub background: Color,
-    pub foreground: Color,
-    pub dark: Color,   // TODO: sort
-    pub border: Color, // TODO: sort
+fn hex_to_color(hex: &str) -> Option<Color> {
+    if hex.len() == 7 {
+        let hash = &hex[0..1];
+        let r = u8::from_str_radix(&hex[1..3], 16);
+        let g = u8::from_str_radix(&hex[3..5], 16);
+        let b = u8::from_str_radix(&hex[5..7], 16);
+
+        return match (hash, r, g, b) {
+            ("#", Ok(r), Ok(g), Ok(b)) => Some(Color {
+                r: r as f32 / 255.0,
+                g: g as f32 / 255.0,
+                b: b as f32 / 255.0,
+                a: 1.0,
+            }),
+            _ => None,
+        };
+    }
+
+    None
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct NormalColors {
-    pub primary: Color,
-    pub secondary: Color,
-    pub surface: Color,
-    pub error: Color,
-}
+pub mod palette_serde {
+    use super::{hex_to_color, Palette};
+    use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct BrightColors {
-    pub primary: Color,
-    pub secondary: Color,
-    pub surface: Color,
-    pub error: Color,
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    struct ThemeHex {
+        background: String,
+        middleground: String,
+        foreground: String,
+        border: String,
+        text: String,
+        accent: String,
+        error: String,
+        warning: String,
+        success: String,
+    }
+
+    impl Serialize for Palette {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            fn as_hex(color: iced_core::Color) -> String {
+                format!(
+                    "#{:02x}{:02x}{:02x}",
+                    (255.0 * color.r).round() as u8,
+                    (255.0 * color.g).round() as u8,
+                    (255.0 * color.b).round() as u8
+                )
+            }
+
+            ThemeHex {
+                background: as_hex(self.background),
+                middleground: as_hex(self.middleground),
+                foreground: as_hex(self.foreground),
+                border: as_hex(self.border),
+                text: as_hex(self.text),
+                accent: as_hex(self.accent),
+                error: as_hex(self.error),
+                warning: as_hex(self.warning),
+                success: as_hex(self.success),
+            }
+            .serialize(serializer)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Palette {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            let hex: ThemeHex = serde::Deserialize::deserialize(deserializer)?;
+
+            let to_color =
+                |hex: &str| hex_to_color(hex).ok_or_else(|| serde::de::Error::custom("Invalid hex"));
+
+            Ok(Palette {
+                background: to_color(&hex.text)?,
+                middleground: to_color(&hex.middleground)?,
+                foreground: to_color(&hex.foreground)?,
+                border: to_color(&hex.border)?,
+                text: to_color(&hex.text)?,
+                accent: to_color(&hex.accent)?,
+                error: to_color(&hex.error)?,
+                warning: to_color(&hex.warning)?,
+                success: to_color(&hex.success)?,
+            })
+        }
+    }
 }
