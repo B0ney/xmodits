a482dfc178e7accc3995251663e00c143f7d16bd
B0ney <40839054+B0ney@users.noreply.github.com>
2023-11-14 19:46:31 +0000

    fix fonts

--- src/font.rs
+++ src/font.rs
@@ -3,13 +3,13 @@ use iced::font::{Font, Family, Weight};
 pub static JETBRAINS_MONO: Font = Font {
     family: Family::Monospace,
     weight: Weight::Medium,
-    ..Font::with_name("JetBrains Mono")
+    ..Font::with_name("JetBrainsMonoNL-Medium")
 };
 
 pub static ICONS: Font = Font::with_name("icons");
 
 pub mod bytes {
-    pub static JETBRAINS_MONO: &[u8] = include_bytes!("../assets/font/JetBrainsMono-Regular.ttf");
+    pub static JETBRAINS_MONO: &[u8] = include_bytes!("../assets/font/JetBrainsMonoNL-Medium.ttf");
     pub static ICONS: &[u8] = include_bytes!("../assets/font/icons.ttf");
     pub static ICED_AW_ICONS: &[u8] = iced_aw::graphics::icons::ICON_FONT_BYTES;
 }
