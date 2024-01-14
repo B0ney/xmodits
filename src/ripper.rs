9ce30a760c261694193c89ac9c03490a67c4f3af
B0ney <40839054+B0ney@users.noreply.github.com>
2024-01-14 21:14:02 +0000

    remove unused "filter" code from ripper

--- src/ripper.rs
+++ src/ripper.rs
@@ -1,13 +1,11 @@
 //! The soul of XMODITS
 
-pub mod filter;
 pub mod handle;
 pub mod signal;
 pub mod stop_flag;
 pub mod subscription;
 pub mod extraction;
 
-pub use filter::CustomFilter;
 pub use signal::Signal;
 pub use handle::Handle;
 pub use extraction::strict_loading;
--- src/ripper/filter.rs
+++
@@ -1,5 +0,0 @@
-use std::path::Path;
-
-pub trait CustomFilter: Fn(&Path) -> bool + Send + Sync {}
-
-impl<T: Fn(&Path) -> bool + Send + Sync> CustomFilter for T {}
