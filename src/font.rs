48b827577bc9a614c76629b9a6ef4b839846cd17
B0ney <40839054+B0ney@users.noreply.github.com>
2024-01-15 15:52:13 +0000

    update font

--- src/font.rs
+++ src/font.rs
@@ -1,16 +1,11 @@
 use iced::font::{self, Family, Font, Weight};
 use iced::Command;
 
-pub static JETBRAINS_MONO: Font = Font {
-    weight: Weight::Medium,
-    ..Font::MONOSPACE
-    // ..Font::with_name("JetBrainsMono NL Medium")
-};
-
+pub static JETBRAINS_MONO: Font = Font::with_name("JetBrains Mono NL");
 pub static ICONS: Font = Font::with_name("icons");
 
 pub mod bytes {
-    pub static JETBRAINS_MONO: &[u8] = include_bytes!("../assets/font/JetBrainsMonoNL-Medium.ttf");
+    pub static JETBRAINS_MONO: &[u8] = include_bytes!("../assets/font/JetBrainsMonoNL-Regular.ttf");
     pub static ICONS: &[u8] = include_bytes!("../assets/font/icons.ttf");
     // pub static ICED_AW_ICONS: &[u8] = iced_aw::graphics::icons::ICON_FONT_BYTES;
 }
