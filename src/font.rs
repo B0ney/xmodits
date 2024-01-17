70e0589e309f98286c9935b255154f30fd134b45
B0ney <40839054+B0ney@users.noreply.github.com>
2024-01-17 19:11:53 +0000

    remove iced_aw font

--- src/font.rs
+++ src/font.rs
@@ -7,7 +7,6 @@ pub static ICONS: Font = Font::with_name("icons");
 pub mod bytes {
     pub static JETBRAINS_MONO: &[u8] = include_bytes!("../assets/font/JetBrainsMonoNL-Regular.ttf");
     pub static ICONS: &[u8] = include_bytes!("../assets/font/icons.ttf");
-    // pub static ICED_AW_ICONS: &[u8] = iced_aw::graphics::icons::ICON_FONT_BYTES;
 }
 
 pub fn load() -> Command<Result<(), font::Error>> {
